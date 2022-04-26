4b64cdee451611eaf4bf197563874aa378605275
Ramsoa <oahmed8@uic.edu>
2022-04-25 22:43:45 -0500

    home stretch

---
+++ en.py
@@ -0,0 +1,44 @@
+en = {
+    "eng": "english",
+    "spa": "spanish",
+
+    "menu": {
+        "greeting" : "\nHello!\nWelcome to Shipbob AR",
+        "modeSelect": "Please select work mode\nPress (x) times to select\n",
+        "modeOpt": "(1) Picker mode\n(2) Settings\n",
+        "inv" : "invalid input",
+    },
+
+     "picker" :{
+        "select" : "Picker Mode\npress (x) times to select\n",
+        "options": "(1) start shift\n(2) mode selection\n",
+        "rtoMode" : "return to mode selection",
+    },
+
+    "shift" : {
+        "start" : "shift started",
+        "scanSlct" : "(1) scanning mode",
+        "scanningMode" : "scanning mode",
+        "ready" : "ready for scan",
+        
+    },
+
+
+
+    "settings" : {
+        "options" : "settings: (1) language (2) unit (3) exit\n",
+        "languages" : "select language:(1) english, (2) spanish\n",
+        "enSelected" : " english selected",
+        "spaSelected" : "spanish selected",
+        "units" : "select unit: (1)imperial(lb) (2)metric\n",
+        "ImpSelected" : "selected imperial",
+        "MetSelected" : "selected metric",
+
+        "inv" : "invalid input",
+    },
+
+
+
+
+
+}
---
+++ items.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+class Item:
+    name: str = "item name"
+    id: int
+    qty: int
+    description = "description"
+    location: str = "location"
+    handling: str = "Default"
+
+
+pick1 = Item('Raspberry pi 7in. touch display', 1001,1, 
+"A touch screen display for raspberry pi in a xbyx box", 
+"Aisle 1, Slot 3")
+
+pick2 = Item('Watch', 1002, 2, "Casio men's watch", "Aisle 2, slot 6")
+
+pick3 = Item('Glass vase', 1006, 1, "glass vase brand etc", "Aisle 1, slot 2")
